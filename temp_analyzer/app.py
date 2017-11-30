d3697eca38749addb030038aa88009b61f255aac
cnucera <claudionucera@gmail.com>
2017-11-30 23:13:13 +0000

    - minor modifications for plotting - saved chart

--- temp_analyzer/app.py
+++ temp_analyzer/app.py
@@ -9,15 +9,17 @@ from temp_analyzer.mongo_manager import MongoManager
 def get_temp():
     m = MongoManager()
     df, df2 = m.get_temp_df()
-    df = df.set_index('timestamp')
-    x = [(i, datetime.time(i)) for i in range(8, 20)]
-
-    df3 = pd.merge(df2.reset_index(), df.reset_index(), on='timestamp', how='outer')
-    df3 = df3.set_index('timestamp')
-    df3 = df3.drop('index', 1)
-    df3.plot()
+    df.index = df['timestamp']
+    df2.index = df2['timestamp']
+    del df['timestamp'], df2['timestamp']
+    df.columns = ['Temperature1']
+    df2.columns = ['Temperature2']
+    ax = df.plot()
+    df2.plot(ax=ax)
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+               ncol=2, mode="expand", borderaxespad=0.)
+    plt.savefig('temp_charts.jpg')
     plt.show()
-
 
 if __name__ == '__main__':
     get_temp()
